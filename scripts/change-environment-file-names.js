f1162116900b053563795fd3199510139648fc14
fernando <fernandomoran323@gmail.com>
2025-03-20 03:56:03 -0700

    config: created scripts/ directory for development utility scripts

---
+++ scripts/change-environment-file-names.js
@@ -0,0 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const environmentsPath = path.join(__dirname, "../src/environments");
+
+function renameFileIfNotExist(source, destination) {
+  const sourcePath = path.join(environmentsPath, source);
+  const destinationPath = path.join(environmentsPath, destination);
+
+  if (!fs.existsSync(destinationPath)) {
+    fs.renameSync(sourcePath, destinationPath);
+  }
+}
+
+renameFileIfNotExist("environments.production.example.ts", "environments.production.ts");
+renameFileIfNotExist("environments.example.ts", "environments.ts");
