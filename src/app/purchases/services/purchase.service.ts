9470f96a9897f282e18e3f6d9933f9aeb366defe
Fernando Jose Moran Arita <fernandomoran323@gmail.com>
2025-07-20 16:39:03 -0700

    feat: added proxy environment for redirect request to different backends

--- src/app/purchases/services/purchase.service.ts
+++ src/app/purchases/services/purchase.service.ts
@@ -1,12 +1,6 @@
 import { BackendProxy } from "@/app/shared/types/backend-proxy";
 import { environments } from "@/environments/environments";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-
-import AuthHttpError from "@/app/auth/errors/auth-http-error";
-import LocalStorageProperties from "@/app/shared/constants/local-storage-properties";
-import { environments } from "@/environments/environments";
-import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
-
 import { Injectable, inject } from "@angular/core";
 import { Observable, catchError, map } from "rxjs";
 import { IPurchaseService, Purchase, PurchaseResponse } from "../interfaces/purchase";
@@ -28,21 +22,18 @@ export default class PurchaseService implements IPurchaseService {
   public createPurchase(purchase: Purchase): Observable<PurchaseResponse> {
     return this._httpClient
       .post<PurchaseResponse>(BACKEND_API.concat("/purchase/create"), purchase, { headers: this._DEFAULT_HEADERS })
-      .post<PurchaseResponse>(BACKEND_URL.concat("/purchase/create"), purchase, { headers: this._DEFAULT_HEADERS })
       .pipe(catchError(AuthHttpError.handler));
   }
 
   public updatePurchase(purchase: Purchase): Observable<PurchaseResponse> {
     return this._httpClient
       .put<PurchaseResponse>(BACKEND_API.concat(`/purchase/update`), purchase, { headers: this._DEFAULT_HEADERS })
-      .put<PurchaseResponse>(BACKEND_URL.concat(`/purchase/update`), purchase, { headers: this._DEFAULT_HEADERS })
       .pipe(catchError(AuthHttpError.handler));
   }
 
   public deletePurchase(id: number): Observable<boolean> {
     return this._httpClient
       .delete<PurchaseResponse>(BACKEND_API.concat(`/purchase/delete/${id}`), { headers: this._DEFAULT_HEADERS })
-      .delete<PurchaseResponse>(BACKEND_URL.concat(`/purchase/delete/${id}`), { headers: this._DEFAULT_HEADERS })
       .pipe(
         catchError(AuthHttpError.handler),
         map((response) => (response.response ? true : false)),
@@ -52,21 +43,18 @@ export default class PurchaseService implements IPurchaseService {
   public getPurchaseById(id: number): Observable<PurchaseResponse> {
     return this._httpClient
       .get<PurchaseResponse>(BACKEND_API.concat(`/purchase/${id}`), { headers: this._DEFAULT_HEADERS })
-      .get<PurchaseResponse>(BACKEND_URL.concat(`/purchase/${id}`), { headers: this._DEFAULT_HEADERS })
       .pipe(catchError(AuthHttpError.handler));
   }
 
   public searchPurchase(term: string): Observable<PurchaseResponse> {
     return this._httpClient
       .get<PurchaseResponse>(BACKEND_API.concat(`/purchase/search/${term}`), { headers: this._DEFAULT_HEADERS })
-      .get<PurchaseResponse>(BACKEND_URL.concat(`/purchase/search/${term}`), { headers: this._DEFAULT_HEADERS })
       .pipe(catchError(AuthHttpError.handler));
   }
 
   public getAll(): Observable<Purchase[]> {
     return this._httpClient
       .get<PurchaseResponse>(BACKEND_API.concat("/purchase/getAll"), { headers: this._DEFAULT_HEADERS })
-      .get<PurchaseResponse>(BACKEND_URL.concat("/purchase/getAll"), { headers: this._DEFAULT_HEADERS })
       .pipe(
         catchError(AuthHttpError.handler),
         map((response) => response.response as Purchase[]),
