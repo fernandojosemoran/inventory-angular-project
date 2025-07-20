08f3c881cdbf81ba65a54fb3bc8077464750abc2
Fernando Jose Moran Arita <fernandomoran323@gmail.com>
2025-07-20 14:27:04 -0700

    feat: added proxy environment for redirect request to different backends

--- src/environments/environments.testing.example.ts
+++ src/environments/environments.testing.example.ts
@@ -1,4 +1,9 @@
 export const environments = {
   backendUrl: "https://url-de-backend.com",
   backendApi: "https://url-de-backend.com/api/v1",
+
+  proxy: "WEBSITE",
+
+  backendApiWebsite: "https://simplified-inventory-management.website/api/v1",
+  backendUrlWebsite: "https://simplified-inventory-management.website",
 };
